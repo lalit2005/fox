e80dd9850c3dd31b3c687822c024f96d28691353
lalit <lalitvijay9480@gmail.com>
2024-10-21 20:16:26 +0530

    finish lexer

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Fox - A Toy Programming Language
+
+Fox is a toy programming language implemented as part of the "Crafting Interpreters" book by Robert Nystrom. This project is a Java-based implementation of the Lox language described in the book.
---
+++ sh/lalit/fox/Scanner.java
@@ -0,0 +1,209 @@
+package sh.lalit.fox;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<Token>();
+    private int start;
+    private int current; // the cursor. "abcd" => if the current is at b, advance returns b and goes to c
+    private int line;
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.CLASS);
+        keywords.put("false", TokenType.CLASS);
+        keywords.put("for", TokenType.CLASS);
+        keywords.put("fun", TokenType.CLASS);
+        keywords.put("if", TokenType.CLASS);
+        keywords.put("nil", TokenType.CLASS);
+        keywords.put("or", TokenType.CLASS);
+        keywords.put("print", TokenType.CLASS);
+        keywords.put("return", TokenType.CLASS);
+        keywords.put("super", TokenType.CLASS);
+        keywords.put("this", TokenType.CLASS);
+        keywords.put("true", TokenType.CLASS);
+        keywords.put("while", TokenType.CLASS);
+        keywords.put("var", TokenType.CLASS);
+    }
+
+    public Scanner(String source) {
+        this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            // cursor at start of next lexeme
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    public boolean scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case ')':
+                addToken(TokenType.RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(TokenType.LEFT_BRACE);
+                break;
+            case '}':
+                addToken(TokenType.RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(TokenType.COMMA);
+                break;
+            case '.':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case '-':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case '+':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case ';':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case '*':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case '!':
+                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+            case '>':
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"':
+                string();
+                break;
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd())
+                        advance();
+                } else {
+                    addToken(TokenType.SLASH);
+                }
+                break;
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Fox.error(line, "Unexpected character");
+                }
+                break;
+        }
+        return false;
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private char peek() {
+        if (isAtEnd())
+            return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekPeek() {
+        if (current + 1 >= source.length())
+            return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected) {
+        if (source.charAt(current) != expected || isAtEnd()) {
+            return false;
+        }
+        current++;
+        return true;
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n')
+                line++;
+            advance();
+        }
+        if (isAtEnd()) {
+            Fox.error(line, "Unterminated string");
+        }
+        advance(); // for the closing "
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number() {
+        while (isDigit(peek()))
+            advance();
+        if (peek() == '.' && isDigit(peekPeek())) {
+            advance();
+            while (isDigit(peek()))
+                advance();
+        }
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_');
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isDigit(c) || isAlpha(c);
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null)
+            type = TokenType.IDENTIFIER;
+        addToken(type);
+    }
+}
